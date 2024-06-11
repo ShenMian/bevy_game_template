60bc88190cbaff112e1b59501c3a354174477fd2
ShenMian <sms_school@outlook.com>
2024-06-11 17:10:36 +0800

    fix(mobile): disable MSAA on Android to prevent device panic
    MSAA (Multi-Sample Anti-Aliasing) has been observed to cause some Android
    devices to panic. This commit disables MSAA on Android builds to avoid
    device crashes while playing the Sokoban game. The issue is under
    investigation and more information can be found in the related Bevy
    engine issue tracker.
    
    Issue: https://github.com/bevyengine/bevy/issues/8229

--- mobile/src/lib.rs
+++ mobile/src/lib.rs
@@ -4,17 +4,23 @@ use sokoban::GamePlugin;
 
 #[bevy_main]
 fn main() {
-    App::new()
-        .add_plugins((
-            DefaultPlugins.set(WindowPlugin {
-                primary_window: Some(Window {
-                    resizable: false,
-                    mode: WindowMode::BorderlessFullscreen,
-                    ..default()
-                }),
+    let mut app = App::new();
+    app.add_plugins((
+        DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                resizable: false,
+                mode: WindowMode::BorderlessFullscreen,
                 ..default()
             }),
-            GamePlugin,
-        ))
-        .run()
+            ..default()
+        }),
+        GamePlugin,
+    ));
+
+    // MSAA makes some Android devices panic, this is under investigation
+    // https://github.com/bevyengine/bevy/issues/8229
+    #[cfg(target_os = "android")]
+    app.insert_resource(Msaa::Off);
+
+    app.run();
 }
